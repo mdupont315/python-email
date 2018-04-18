7c26014bf162b77be54e1c9e509bf98d501a4f0d
Jhonatan <mollitiamteodoro@gmail.com>
2018-04-17 23:22:01 -0300

    Envio de email simples - beta

--- myproject/core/mixins.py
+++ myproject/core/mixins.py
@@ -11,31 +11,27 @@ class EmailMixin:
     email_subject = ''
 
     def send_mail(self):
+        """
+        Método para envio de email seguindo padrão send_mail do django
+        """
         subject = self.email_subject
         from_ = self.email_from
         to = self.get_email_to()
         template_name = self.get_email_template_name()
         context = self.get_email_context_data()
         body = render_to_string(template_name, context)
-        return mail.send_mail(subject, body, from_, [from_, to])
+        return mail.send_mail(subject, body, from_, [to])
 
     def get_email_template_name(self):
         if self.email_template_name:
             return self.email_template_name
-        meta = self.object._meta
-        return '{}/{}_email.txt'.format(meta.app_label, meta.model_name)
+        return None
 
-    def get_email_context_data(self, **kwargs):
-        context = dict(kwargs)
-        context.setdefault(self.get_email_context_name(), self.object)
+    def get_email_context_data(self):
+        context = self.get_context_data()
         return context
 
-    def get_email_context_name(self):
-        if self.email_context_name:
-            return self.email_context_name
-        return self.object._meta.model_name
-
     def get_email_to(self):
-        if self.email_to:
-            return self.email_to
-        return self.object.email
+        if self.request.POST.get('email_to'):
+            return self.request.POST.get('email_to')
+        return 'none'
